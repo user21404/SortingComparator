945c2e45584480d00150fafff2a3e30c65895c00
el-diavolo-07 <60542413+el-diavolo-07@users.noreply.github.com>
2021-10-20 17:53:34 +0530

    Bubble-Sort.java

--- src/algo-files/Bubble-Sort.js
+++ src/algo-files/Bubble-Sort.js
@@ -1,20 +1,37 @@
-function swap(arr, firstIndex, secondIndex) {
-  var temp = arr[firstIndex];
-  arr[firstIndex] = arr[secondIndex];
-  arr[secondIndex] = temp;
+// Java program for implementation of Bubble Sort
+class BubbleSort
+{
+	void bubbleSort(int arr[])
+	{
+		int n = arr.length;
+		for (int i = 0; i < n-1; i++)
+			for (int j = 0; j < n-i-1; j++)
+				if (arr[j] > arr[j+1])
+				{
+					// swap arr[j+1] and arr[j]
+					int temp = arr[j];
+					arr[j] = arr[j+1];
+					arr[j+1] = temp;
+				}
+	}
+
+	/* Prints the array */
+	void printArray(int arr[])
+	{
+		int n = arr.length;
+		for (int i=0; i<n; ++i)
+			System.out.print(arr[i] + " ");
+		System.out.println();
+	}
+
+	// Driver method to test above
+	public static void main(String args[])
+	{
+		BubbleSort ob = new BubbleSort();
+		int arr[] = {64, 34, 25, 12, 22, 11, 90};
+		ob.bubbleSort(arr);
+		System.out.println("Sorted array");
+		ob.printArray(arr);
+	}
 }
-function bubbleSortAlgo(arraaytest) {
-  var len = arraaytest.length,
-    i,
-    j,
-    stop;
-  for (i = 0; i < len; i++) {
-    for (j = 0, stop = len - i; j < stop; j++) {
-      if (arraaytest[j] > arraaytest[j + 1]) {
-        swap(arraaytest, j, j + 1);
-      }
-    }
-  }
-  return arraaytest;
-}
-console.log(bubbleSortAlgo([11, 60, 12, 15, 95, 44, 61]));
+/* This code is contributed by Rajat Mishra */
